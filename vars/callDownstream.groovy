09d61a8e362b27af78240f5fedc42a9a7895dff6
Jason Dearth <jdearth@discussoftware.com>
2020-04-20 16:16:35 -0400

    check for snapshot or master branch

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(String repoName) {
-    def jobName = "../${repoName}/${env.BRANCH_NAME}"
-    build job: jobName, propagate: false, wait: false
+    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.contains("SNAPSHOT")) {
+        def jobName = "../${repoName}/${env.BRANCH_NAME}"
+        build job: jobName, propagate: false, wait: false
+    }
 }
--- vars/performArchival.groovy
+++ vars/performArchival.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(boolean includeTestReports) {
+def call(boolean includeTestReports = true) {
     if (includeTestReports) {
         junit '**/target/surefire-reports/**/*.xml'
     }
