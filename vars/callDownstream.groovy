cacdc398e15859b1e995cfa3a583cd84090ea630
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 14:05:00 -0400

    convert to comma delimited string parameter

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String[] repoNames) {
+def call(String repoNames) {
     echo repoName
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
-        for (String repoName : repoNames) {
+        String[] str = repoNames.split(',')
+        for (String repoName : str) {
             def jobName = "../${repoName}/${env.BRANCH_NAME}"
             build job: jobName, propagate: false, wait: false
         }
