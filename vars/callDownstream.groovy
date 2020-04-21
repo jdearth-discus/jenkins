b0b9627bf4e9bdcdb15fc93e9359b9e04e577ae4
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 10:08:47 -0400

    print statement

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String repoName) {
+    echo repoName
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
         def jobName = "../${repoName}/${env.BRANCH_NAME}"
         build job: jobName, propagate: false, wait: false
