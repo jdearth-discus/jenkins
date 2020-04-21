751f0a447225a1e654ad908fe9c6a520d0e684ef
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 08:36:46 -0400

    fix syntax bug

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String[] repoNames) {
-    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT") {
+    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
         for (repoName in repoNames) {
             def jobName = "../${repoName}/${env.BRANCH_NAME}"
             build job: jobName, propagate: false, wait: false
