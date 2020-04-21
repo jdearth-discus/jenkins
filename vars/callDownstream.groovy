f4e1aaca1240b57972d608677f0a98161adc1ed7
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 13:20:52 -0400

    convert callDownstream back to list of strings

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String repoName) {
+def call(String[] repoNames) {
     echo repoName
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
-        def jobName = "../${repoName}/${env.BRANCH_NAME}"
-        build job: jobName, propagate: false, wait: false
+        for (String repoName : repoNames) {
+            def jobName = "../${repoName}/${env.BRANCH_NAME}"
+            build job: jobName, propagate: false, wait: false
+        }
     } else {
         echo "This is a feature branch and will not build downstream"
     }
