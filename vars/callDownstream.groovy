eb254f2ac0659159c6223985bc8a5a901677b164
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 14:37:23 -0400

    just call method multiple times if needed

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,13 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String repoNames) {
-    echo repoName
+def call(String repoName) {
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
-        String[] str = repoNames.split(',')
-        for (String x : str) {
-            def jobName = "../" + x + "/${env.BRANCH_NAME}"
-            build job: jobName, propagate: false, wait: false
-        }
+        def jobName = "../${repoName}/${env.BRANCH_NAME}"
+        build job: jobName, propagate: false, wait: false
     } else {
         echo "This is a feature branch and will not build downstream"
     }
