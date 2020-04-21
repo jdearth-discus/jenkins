1fa9094790c832d1e8a76e1559e6e33c55f1dd7c
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 09:04:53 -0400

    change callDownstream back to check for SNAPSHOT

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String repoName) {
-    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("jenkinsfile")) {
+    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
         def jobName = "../${repoName}/${env.BRANCH_NAME}"
         build job: jobName, propagate: false, wait: false
     }
