890425751e298a15ba42516bd7c4ad751658dc62
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 08:43:22 -0400

    remove list from callDownstream

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String[] repoNames) {
-    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("SNAPSHOT")) {
-        for (repoName in repoNames) {
-            def jobName = "../${repoName}/${env.BRANCH_NAME}"
-            build job: jobName, propagate: false, wait: false
-        }
+def call(String repoNames) {
+    if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("jenkinsfile")) {
+        def jobName = "../${repoName}/${env.BRANCH_NAME}"
+        build job: jobName, propagate: false, wait: false
     }
 }
