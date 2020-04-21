4d99f9522708bc3e2f49bca7c8ca64647413856c
Jason Dearth <jdearth@discussoftware.com>
2020-04-21 08:47:00 -0400

    bug fix

--- vars/callDownstream.groovy
+++ vars/callDownstream.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String repoNames) {
+def call(String repoName) {
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME.endsWith("jenkinsfile")) {
         def jobName = "../${repoName}/${env.BRANCH_NAME}"
         build job: jobName, propagate: false, wait: false
